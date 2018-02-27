201674e0d0fff59025174b41c8fb521450a9f3a5
Kirill Grischenko <grischenkokiril@gmail.com>
2018-02-27 15:40:21 +0200

    add comments

--- server/middlewares/catchErrors.js
+++ server/middlewares/catchErrors.js
@@ -1,20 +1,23 @@
 import logger from '../log';
 
 export default (err, req, res, next) => {
+  // if error is boom
   if (err.output) {
+    // if boom error is server error
     if (err.isServer) {
       res.status(err.output.statusCode);
       res.send('Неудалось выполнить запрос, попробуйте перезагрузить страницу и попробовать снова');
       
       return res.on('finish', () => logger.error(err.stack));
     };
+    // if boom error is http error
     const { statusCode, message } = err.output.payload;
     
     res.status(statusCode);
     res.send(message);
     
     return res.on('finish', () => logger.data(message));
-   }
-
+  };
+  // other errors
   return log.warn(err.stack);
 };
