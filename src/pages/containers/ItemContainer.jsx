183d1dbc41f5293c30c45741eee30eb2561b4e4d
Kirill Grischenko <grischenkokiril@gmail.com>
2018-03-01 16:36:51 +0200

    delete shuffle function

--- src/pages/containers/ItemContainer.jsx
+++ src/pages/containers/ItemContainer.jsx
@@ -3,17 +3,17 @@ import { connect } from 'react-redux';
 import Item from '../components/Item';
 
 import { getProductsForItemPage, addToCart } from '../../actions';
-import { shuffleProducts } from '../../reducers';
+// import { shuffleProducts } from '../../reducers';
 
 function mapStateToProps(state, { match }) {
   const { category, id } = match.params;
-  const { loading, item } = state.shop;
+  const { loading, products, item } = state.shop;
 
   return ({
     category,
     id,
     loading,
-    products: shuffleProducts(state),
+    products,
     item,
   });
 }
